b036e7661561fa3080414af1e3d1f6f06c2a5e33
Robyn Choi <robyn3choi@gmail.com>
2023-10-24 20:30:48 -0700

    favicon and meta tags

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="text-yellow p-4 mb-12 sm:mb-0 absolute bottom-1 text-center w-full">
-      This is an online dice roller for{' '}
+      This is an online dice roller and clock tracker for{' '}
       <a
         className="underline hover:text-white"
         href="https://jexjthomas.itch.io/bump-in-the-dark"
